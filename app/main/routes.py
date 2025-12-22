003df286af0ef1fb44b4e19544ba9a6487143d1c
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2025-12-22 12:30:27 -0500

    feat(ui): Display recent public soundboards on home page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app.main import bp
+from app.models import Soundboard
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    sbs = Soundboard.get_recent_public(limit=6)
+    return render_template('index.html', title='Home', soundboards=sbs)
